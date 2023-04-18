b775d60b1d0edc3bc3e80282c38b01b816c8ef81
joni ursin <joni.ursin@gmail.com>
2023-04-18 18:51:39 +0300

    added coins

--- pacman/src/level.py
+++ pacman/src/level.py
@@ -2,6 +2,8 @@ import pygame
 from sprites.player import Player
 from sprites.box import Box
 from sprites.floor import Floor
+from sprites.coin import Coin
+
 
 class Level:
     def __init__(self, level_map, cell_size):
@@ -9,6 +11,7 @@ class Level:
         self.player = None
         self.boxes = pygame.sprite.Group()
         self.floors = pygame.sprite.Group()
+        self.coins = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
 
         self._initialize_sprites(level_map)
@@ -30,12 +33,16 @@ class Level:
                 elif cell == 2:
                     self.player = Player(normalized_x, normalized_y)
                     self.floors.add(Floor(normalized_x, normalized_y))
+                elif cell == 3:
+                    self.coins.add(Coin(normalized_x, normalized_y))
+                    self.floors.add(Floor(normalized_x, normalized_y))
 
-        self.all_sprites.add(self.floors, self.boxes, self.player)
+        self.all_sprites.add(self.floors, self.boxes, self.coins, self.player)
 
     def _check_move(self, x=0, y=0):
         self.player.rect.move_ip(x, y)
-        colliding_walls = pygame.sprite.spritecollide(self.player, self.boxes, False)
+        colliding_walls = pygame.sprite.spritecollide(
+            self.player, self.boxes, False)
         can_move = not colliding_walls
 
         self.player.rect.move_ip(-x, -y)
