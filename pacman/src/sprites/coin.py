349bd2c6237ffadcd2360ee44ef589e9afbe5d8c
joni ursin <joni.ursin@gmail.com>
2023-04-18 18:47:47 +0300

    added coin.py

---
+++ pacman/src/sprites/coin.py
@@ -0,0 +1,15 @@
+import os
+import pygame
+
+dirname = os.path.dirname(__file__)
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, x=0, y=0):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(
+            dirname, "..", "assets", "coin.png"))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        
