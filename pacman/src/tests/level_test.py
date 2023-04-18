f0c51d743e719723794e64868345942e8f646133
joni ursin <joni.ursin@gmail.com>
2023-04-18 21:32:51 +0300

    added tests for coins and death

--- pacman/src/tests/level_test.py
+++ pacman/src/tests/level_test.py
@@ -2,10 +2,10 @@ import unittest
 from level import Level
 
 LEVEL_MAP = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 4, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 2, 0, 0, 0, 0, 0, 0, 0, 1],
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 3, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 3, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -28,8 +28,31 @@ class TestLevel(unittest.TestCase):
 
         self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
 
-        self.level.move_player(x=-CELL_SIZE)
+        self.level.move_player(x_coord=-CELL_SIZE)
         self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
 
-        self.level.move_player(x=CELL_SIZE)
+        self.level.move_player(x_coord=CELL_SIZE)
         self.assert_coordinates_equal(player, 2 * CELL_SIZE, 2 * CELL_SIZE)
+
+    def test_player_can_collect_coins(self):
+        player = self.level.player
+
+        self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
+
+        self.assertEqual(self.level.score, 0)
+
+        self.level.move_player(y_coord=CELL_SIZE)
+        self.assertEqual(self.level.score, 1)
+
+        self.level.move_player(y_coord=CELL_SIZE)
+        self.assertEqual(self.level.score, 2)
+
+    def test_player_can_die(self):
+        player = self.level.player
+
+        self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
+
+        self.level.move_player(y_coord=-CELL_SIZE)
+        self.level.check_death()
+        self.assertEqual(self.level.player.alive(), False)
+
