5b6f4c2e1004f55408a02f1013e95a83e9e0cfc7
joni ursin <joni.ursin@gmail.com>
2023-04-25 22:17:54 +0300

    merged teleporter code into single class

--- pacman/src/sprites/teleporter.py
+++ pacman/src/sprites/teleporter.py
@@ -4,11 +4,12 @@ import pygame
 dirname = os.path.dirname(__file__)
 
 
-class TeleporterLeft(pygame.sprite.Sprite):
-    def __init__(self, x_coord=0, y_coord=0):
+class Teleporter(pygame.sprite.Sprite):
+    def __init__(self, x_coord=0, y_coord=0, direction=""):
         super().__init__()
         self.image = pygame.image.load(
             os.path.join(dirname, "..", "assets", "floor.png"))
         self.rect = self.image.get_rect()
         self.rect.x = x_coord
         self.rect.y = y_coord
+        self.direction = direction
