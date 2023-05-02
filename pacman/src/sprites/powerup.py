565eb9f48e7264ad2919043f07a79384e262632b
joni ursin <joni.ursin@gmail.com>
2023-05-02 21:47:14 +0300

    added powerup

---
+++ pacman/src/sprites/powerup.py
@@ -0,0 +1,14 @@
+import os
+import pygame
+
+dirname = os.path.dirname(__file__)
+
+
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self, x_coord=0, y_coord=0):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(
+            dirname, "..", "assets", "powerup.png"))
+        self.rect = self.image.get_rect()
+        self.rect.x = x_coord
+        self.rect.y = y_coord
