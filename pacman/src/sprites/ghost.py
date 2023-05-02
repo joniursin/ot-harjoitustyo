14c98d4724efd9460fac9381edd14f42957cc5c9
joni ursin <joni.ursin@gmail.com>
2023-05-02 23:03:53 +0300

    added docstring

--- pacman/src/sprites/ghost.py
+++ pacman/src/sprites/ghost.py
@@ -6,7 +6,18 @@ dirname = os.path.dirname(__file__)
 
 
 class Ghost(pygame.sprite.Sprite):
+    """Luokka, joka luo kummitus olion
+
+    Args:
+        pygame (pygame.sprite): Luo sprite olion
+    """
     def __init__(self, x_coord=0, y_coord=0):
+        """Luokan konstruktori, joka luo oliolle paikan pelikentällä
+
+        Args:
+            x_coord (int, optional): Maailman x-koordinaatti. Defaults to 0.
+            y_coord (int, optional): Maailman y-koordinaatti. Defaults to 0.
+        """
         super().__init__()
         self.image = pygame.image.load(os.path.join(
             dirname, "..", "assets", "ghost.png"))
@@ -16,6 +27,11 @@ class Ghost(pygame.sprite.Sprite):
         self.direction = random.choice(["N", "S", "W", "E"])
 
     def move(self, cell_size):
+        """Likuuttaa kummitusta sen sunnnan mukaan
+
+        Args:
+            cell_size (int): Pelikentän yhden ruudun koko
+        """
         if self.direction == "N":
             self.rect.move_ip(0, -cell_size)
         elif self.direction == "S":
@@ -26,6 +42,15 @@ class Ghost(pygame.sprite.Sprite):
             self.rect.move_ip(-cell_size, 0)
 
     def check_move(self, cell_size, boxes):
+        """Tarkistaa kaikki kummituksen mahdolliset liikkeet
+
+        Args:
+            cell_size (int): Pelikentän yhden ruudun koko
+            boxes (pygame.sprite): Lista seinistä, jotka estävät liikkeen
+
+        Returns:
+            list: Palauttaa kaikki mahdolliset liikkumis suunnat
+        """
         exits = []
 
         self.rect.move_ip(cell_size, 0)
@@ -51,6 +76,12 @@ class Ghost(pygame.sprite.Sprite):
         return exits
 
     def choose_move(self, cell_size, boxes):
+        """Valitsee kummitukselle seuraavan liikkeen ja suorittaa sen
+
+        Args:
+            cell_size (int): Pelikentän yhden ruudun koko
+            boxes (pygame.sprite): Lista seinistä, jotka estävät liikkeen
+        """
         exits = self.check_move(cell_size, boxes)
         if self.direction in exits and random.randrange(0, 100) <= 50:
             pass
