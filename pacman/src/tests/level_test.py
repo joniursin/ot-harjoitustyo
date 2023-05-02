47c7a8f90e1f7b33a3e7c93a6a272a0f201aa522
joni ursin <joni.ursin@gmail.com>
2023-05-02 23:18:42 +0300

    added tests for power up

--- pacman/src/tests/level_test.py
+++ pacman/src/tests/level_test.py
@@ -1,9 +1,10 @@
 import unittest
 from level import Level
+from clock import Clock
 
 LEVEL_MAP = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 4, 0, 0, 0, 0, 0, 0, 0, 1],
-             [1, 2, 0, 0, 0, 0, 0, 0, 0, 1],
+             [1, 2, 0, 7, 4, 0, 0, 0, 0, 1],
              [1, 3, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 3, 0, 0, 0, 0, 0, 0, 0, 1],
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -13,11 +14,11 @@ LEVEL_MAP = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
 CELL_SIZE = 50
-
+CLOCK = Clock()
 
 class TestLevel(unittest.TestCase):
     def setUp(self):
-        self.level = Level(LEVEL_MAP, CELL_SIZE)
+        self.level = Level(LEVEL_MAP, CELL_SIZE, CLOCK)
 
     def assert_coordinates_equal(self, sprite, x, y):
         self.assertEqual(sprite.rect.x, x)
@@ -68,3 +69,38 @@ class TestLevel(unittest.TestCase):
         self.level.move_player(y_coord=-CELL_SIZE)
         self.level.check_death()
         self.assertEqual(self.level.get_lives(), 2)
+
+    def test_powerup_pickup(self):
+        
+        player = self.level.player
+
+        self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
+
+        self.assertEqual(self.level.get_powerup_status(), False)
+
+        self.level.move_player(x_coord=CELL_SIZE)
+        self.level.check_powerup()
+        self.assertEqual(self.level.get_powerup_status(), False)
+
+        self.level.move_player(x_coord=CELL_SIZE)
+        self.level.check_powerup()
+        self.assertEqual(self.level.get_powerup_status(), True)
+
+    def test_powerup_kills_enemies(self):
+
+        player = self.level.player
+
+        self.assertEqual(self.level.get_lives(), 3)
+        
+        self.assert_coordinates_equal(player, CELL_SIZE, 2 * CELL_SIZE)
+
+        self.assertEqual(self.level.get_powerup_status(), False)
+
+        self.level.move_player(x_coord=CELL_SIZE)
+        self.level.move_player(x_coord=CELL_SIZE)
+        self.level.check_powerup()
+        self.assertEqual(self.level.get_powerup_status(), True)
+
+        self.level.move_player(x_coord=CELL_SIZE)
+        self.level.check_death()
+        self.assertEqual(self.level.get_lives(), 3)
