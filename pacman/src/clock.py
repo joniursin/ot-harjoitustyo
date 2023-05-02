00783e61022947d3f859a2d455e1c78818a8b708
joni ursin <joni.ursin@gmail.com>
2023-05-02 22:19:49 +0300

    added docstring

--- pacman/src/clock.py
+++ pacman/src/clock.py
@@ -2,11 +2,25 @@ import pygame
 
 
 class Clock:
+    """Luokka, jota käytetään pelin kellona
+    """
     def __init__(self):
+        """Luokan konstruktori, joka luo kellon
+        """
         self._clock = pygame.time.Clock()
 
     def tick(self, fps):
+        """Pitää huolen, että peli kulkee tietyllä fps määrällä
+
+        Args:
+            fps (int): Pelin fps määrä
+        """
         self._clock.tick(fps)
 
     def get_ticks(self):
+        """Palauttaa kuluneen ajan käynnistyksestä
+
+        Returns:
+            int: Kulunut aika käynnistyksestä
+        """
         return pygame.time.get_ticks()
--- pacman/src/eventqueue.py
+++ pacman/src/eventqueue.py
@@ -2,5 +2,12 @@ import pygame
 
 
 class EventQueue:
+    """Luokka, joka pitää huolen pelin tapahtumista
+    """
     def get(self):
+        """Palauttaa pelaajan näppäinpainalmukset
+
+        Returns:
+            pygame.event: Palauttaa tapahtumat
+        """
         return pygame.event.get()
