cb270d973440f7de2db039739374a604d5356d06
joni ursin <joni.ursin@gmail.com>
2023-05-02 21:48:25 +0300

    added main menu and power up texts

--- pacman/src/renderer.py
+++ pacman/src/renderer.py
@@ -16,11 +16,6 @@ class Renderer:
             "Score: " + str(Level.get_score(self._level)), True, (255, 255, 255))
         self._display.blit(score_text, (5, 10))
 
-        # Draw game over
-        if not Level.get_player(self._level).alive():
-            score_text = font.render("Game Over!", True, (255, 255, 255))
-            self._display.blit(score_text, (160, 10))
-
         if Level.get_lives(self._level) > 0:
             score_text = font.render(
                 "Lives: " + str(Level.get_lives(self._level)), True, (255, 255, 255))
@@ -29,5 +24,15 @@ class Renderer:
         score_text = font.render(
             "Level: " + str(Level.get_level(self._level)), True, (255, 255, 255))
         self._display.blit(score_text, (460, 10))
+
+        if Level.get_powerup_status(self._level):
+            score_text = font.render(
+            "Power up activated for 5 seconds!", True, (255, 255, 255))
+            self._display.blit(score_text, (140, 620))
+        # Draw game over
+        if not Level.get_player(self._level).alive():
+            self._display.fill((0, 0, 0))
+            score_text = font.render("Game Over! Press 'e' to go back to the main menu", True, (255, 255, 255))
+            self._display.blit(score_text, (30, 300))
 
         pygame.display.update()
